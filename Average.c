d4594e61ddf504dd208e3fd08f24431cd8ec59c6
Rhutu Sawant <rhutusawant@gmail.com>
2026-08-19 22:32:35 +0530

    Average.c

--- Average.c
+++ Average.c
@@ -4,7 +4,7 @@ void main()
     int a, b, c;
     float d;
     printf("Enter three numbers : \n");
-    scanf("%d %d %d ", &a, &b, &c);
+    scanf("%d %d %d", &a, &b, &c);
     d = (a + b + c) / 3.0;
     printf("The average of three numbers is %f ", d);
 }
